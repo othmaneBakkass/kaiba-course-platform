b9a7391c6dcce7f63fbabd304ee5336a3a99a38b
othmane bakkass <othmanebakkass@gmail.com>
2023-12-23 23:41:22 +0100

    feat(providers): add react query provider

---
+++ app/providers/react-query.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import type { ReactNode } from 'react';
+
+const queryClient = new QueryClient();
+
+export default function Provider({ children }: { children: ReactNode }) {
+	return (
+		<>
+			<QueryClientProvider client={queryClient}>
+				{children}
+				{/* <ReactQueryDevtools /> */}
+			</QueryClientProvider>
+		</>
+	);
+}
