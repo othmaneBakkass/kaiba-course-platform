3e31160778a96e1fd42a4083aa428da3c5315d25
othmane bakkass <othmanebakkass@gmail.com>
2023-12-29 21:42:55 +0100

    refactor(use-prefer-reduced-motion): add docs

--- lib/hooks/use-prefer-reduced-motion.ts
+++ lib/hooks/use-prefer-reduced-motion.ts
@@ -3,6 +3,12 @@
 import { useEffect, useState } from 'react';
 
 const QUERY = '(prefers-reduced-motion: no-preference)';
+/**
+ * The `usePrefersReducedMotion` function returns a boolean value indicating whether the user prefers
+ * reduced motion based on their system settings.
+ * @returns The function `usePrefersReducedMotion` returns the value of `prefersReducedMotion`, which
+ * is a boolean indicating whether the user prefers reduced motion.
+ */
 
 export function usePrefersReducedMotion() {
 	const [prefersReducedMotion, setPrefersReducedMotion] = useState(false);
